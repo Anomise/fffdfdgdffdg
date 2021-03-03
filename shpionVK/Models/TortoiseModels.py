d037cb43b64e3b1b27ca3b86afe9d41480aa879f
Anomise <79972271+Anomise@users.noreply.github.com>
2021-03-03 14:10:07 +0300

    Delete shpionVK directory

--- shpionVK/Models/TortoiseModels.py
+++
@@ -1,7 +0,0 @@
-from tortoise import Tortoise,fields
-from tortoise.models import Model
-
-class Users(Model):
-	id = fields.IntField(pk=True)
-	user_id = fields.IntField()
-	balance = fields.IntField(default=0)
--- shpionVK/cfgs/cfg.py
+++
@@ -1,5 +0,0 @@
-from vkbottle import Bot
-from vbml import Patcher
-
-bot = Bot("0ed72b15a3ba7babf0e04f533a1b931268de91cdcced5e8f6dd0ab741994eb4cd46256c4b0e623cbf2a41")
-patcher = Patcher()
